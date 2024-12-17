c789a0f3be50636ac2fdc114207b1d16a1c3debe
Faik, Azzeddine <azzeddine.faik@dice.se>
2024-12-17 13:37:06 +0100

    Tensors

--- pytorch-tensors.py
+++ pytorch-tensors.py
@@ -19,3 +19,19 @@ tensor = torch.rand(3,4)
 print(f"Shape of tensor: {tensor.shape}")
 print(f"Datatype of tensor: {tensor.dtype}")
 print(f"Device tensor is stored on: {tensor.device}")
+
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+  tensor = tensor.to('cuda')
+
+tensor = torch.ones(4, 4)
+print('First row: ',tensor[0])
+print('First column: ', tensor[:, 0])
+print('Last column:', tensor[..., -1])
+# modify
+tensor[:,1] = 0
+print(tensor)
+
+# concatenate multiple tensors
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
